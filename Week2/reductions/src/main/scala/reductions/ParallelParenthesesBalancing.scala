254af8176922e6a44c7073a567ead5da09a7caf4
Zhichao <silverwindcao@gmail.com>
2016-06-10 09:59:20 -0400

    Week2 is unfinished

---
+++ Week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -0,0 +1,80 @@
+package reductions
+
+import scala.annotation._
+import org.scalameter._
+import common._
+
+object ParallelParenthesesBalancingRunner {
+
+  @volatile var seqResult = false
+
+  @volatile var parResult = false
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 40,
+    Key.exec.maxWarmupRuns -> 80,
+    Key.exec.benchRuns -> 120,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val length = 100000000
+    val chars = new Array[Char](length)
+    val threshold = 10000
+    val seqtime = standardConfig measure {
+      seqResult = ParallelParenthesesBalancing.balance(chars)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential balancing time: $seqtime ms")
+
+    val fjtime = standardConfig measure {
+      parResult = ParallelParenthesesBalancing.parBalance(chars, threshold)
+    }
+    println(s"parallel result = $parResult")
+    println(s"parallel balancing time: $fjtime ms")
+    println(s"speedup: ${seqtime / fjtime}")
+  }
+}
+
+object ParallelParenthesesBalancing {
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def balance(chars: Array[Char]): Boolean = {
+    ifBalanced(chars, 0)
+  }
+
+  def ifBalanced(chars: Array[Char], count: Int): Boolean = {
+    if(chars.isEmpty) {
+      count == 0
+    }
+    else if(chars.head == '(') {
+      ifBalanced(chars.tail, count + 1)
+    }
+    else if(chars.head == ')') {
+      count > 0 && ifBalanced(chars.tail, count - 1)
+    }
+    else {
+      ifBalanced(chars.tail, count)
+    }
+  }
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
+
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
+      ???
+    }
+
+    def reduce(from: Int, until: Int) /*: ???*/ = {
+      ???
+    }
+
+    reduce(0, chars.length) == ???
+  }
+
+  // For those who want more:
+  // Prove that your reduction operator is associative!
+
+}
